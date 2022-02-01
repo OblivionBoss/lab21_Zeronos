df6cc8dd714c203b09f289880440380a74ff038d
Zeronos <boss16805@gmail.com>
2022-02-01 16:33:01 +0700

    lab21_2 completed

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,23 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *I, Rect *II){
+	double WI = min(I->x + I->w, II->x + II->w) - max(I->x, II->x);
+    double HI = min(I->y, II->y) - max(I->y - I->h, II->y - II->h);
+    double areaI = 0;
+    if( WI > 0 && HI > 0 )
+    {
+        areaI = WI * HI;
+    }
+     
+    return areaI;
 
 
+}
 
+int main(){
+	Rect I = {1,1,5,5};
+	Rect II = {2,2,5,5};
+	cout << overlap(&I,&II);
+	return 0;
 }
